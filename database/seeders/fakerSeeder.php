11b8a20181c23f961b990c8153fd34308bc4d536
AntonioDattolo <antonio19794.a@gmail.com>
2024-07-03 16:24:58 +0200

    Aggiunta Preview README.MD

--- database/seeders/fakerSeeder.php
+++ database/seeders/fakerSeeder.php
@@ -10,16 +10,19 @@ class fakerSeeder extends Seeder
 {
     public function run(Faker $faker): void
     {
-        $newRecord = new Train();
-        $newRecord->azienda = $faker->company();
-        $newRecord->stazione_di_partenza = $faker->address();
-        $newRecord->stazione_di_arrivo = $faker->address();
-        $newRecord->orario_di_partenza = $faker->datetime();
-        $newRecord->orario_di_arrivo = $faker->datetime();
-        $newRecord->codice_treno = $faker->randomNumber(5, true);
-        $newRecord->numero_di_carrozze = $faker->randomNumber(3, true);
-        $newRecord->in_orario = 1;
-        $newRecord->cancellato = 0;
-        $newRecord->save();
+        for ($i=0; $i < 10 ; $i++) { 
+           
+            $newRecord = new Train();
+            $newRecord->azienda = $faker->company();
+            $newRecord->stazione_di_partenza = $faker->address();
+            $newRecord->stazione_di_arrivo = $faker->address();
+            $newRecord->orario_di_partenza = $faker->datetime();
+            $newRecord->orario_di_arrivo = $faker->datetime();
+            $newRecord->codice_treno = $faker->randomNumber(5, true);
+            $newRecord->numero_di_carrozze = $faker->randomNumber(3, true);
+            $newRecord->in_orario = 1;
+            $newRecord->cancellato = 0;
+            $newRecord->save();
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,28 +4,42 @@
 <section class="container">
     <ul>
 
-        @foreach ($dato as $index => $item)
-        <li>
-           {{$index + 1}} elemento della tabella : <br> {{ $item }} 
-        </li>
-            
-        
-        @endforeach
+        <table>
+            <thead>
+                <tr style="border-bottom:1px solid white">
+                    <th scope="col-1">#</th>
+                    <th scope="col-1">Azienda</th>
+                    <th scope="col-1">Partenza</th>
+                    <th scope="col-1">Arrivo</th>
+                    <th scope="col-1">Orario di partenza</th>
+                    <th scope="col-1">Orario di arrivo</th>
+                    <th scope="col-1">Treno N°</th>
+                    <th scope="col-1">Carrozze</th>
+                    <th scope="col-1">In Orario</th>
+                    <th scope="col-1">Cancellato</th>
+                </tr>
+            </thead>
+            <tbody>
+         @foreach ($dato as $index => $item)
+                <tr>
+                    <th style="border-bottom:1px solid white" scope="row">{{$index + 1}}</th>
+                    <td style="border-bottom:1px solid white">{{$item->azienda}}</td>
+                    <td style="border-bottom:1px solid white">{{$item->stazione_di_partenza}}</td>
+                    <td style="border-bottom:1px solid white">{{$item->stazione_di_arrivo}}</td>
+                    <td style="border-bottom:1px solid white">{{$item->orario_di_partenza}}</td>
+                    <td style="border-bottom:1px solid white">{{$item->orario_di_arrivo}}</td>
+                    <td style="border-bottom:1px solid white">{{$item->codice_treno}}</td> 
+                    <td style="border-bottom:1px solid white">{{$item->numero_di_carrozze}}</td>
+                    <td style="border-bottom:1px solid white">
+                        {{($item->in_orario == true) ? "Nessun ritardo" : "in ritardo"}}
+                    </td>
+                    <td style="border-bottom:1px solid white">
+                        {{($item->cancellato == true) ? "//" : "CANCELLATO"}}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </ul>
-        
-        
-        
-   
-    <p>FUNGE</p>
-    <button class="btn btn-primary">
-        <a href="http://" style="color: whitesmoke">
-            Cliccami
-        </a>
-    </button>
-    <button class="btn btn-primary">
-        <a href="http://" style="color: whitesmoke">
-            Cliccami
-        </a>
-    </button>
 </section>
 @endsection
